f72df641d3e908c8bea7ead23aad83e309f031e5
kwonyongcheol <kyxzoro@gmail.com>
2023-07-10 10:41:50 +0900

    막냉이~ 쪼기!

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,10 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it! start!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <img alt="Vue logo" class="logo" src="./assets/img/yum_cat.png" width="125" height="125" />
+    <p>막냉아~ 고양이 캐릭터 그려줘라!!!</p>
+  </div>
 </template>
 
 <style scoped>
@@ -27,21 +20,4 @@ header {
   margin: 0 auto 2rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
