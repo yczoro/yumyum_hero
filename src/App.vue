d75220509ae9108699007bbfbd777801d0ba260d
sorahah <sorahjjang95@gmail.com>
2023-07-10 18:40:43 +0900

    소라 수정

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <p> -- 여기 밑으로 본인 이름이으로 사인 남길것 ex) 홍길동 연결함! --</p>
     <p>혜원 연결 성공함!<img alt="Vue logo" class="logo" src="./assets/img/yum_heart.png" width="128" height="118" /></p>
     <p>채은짱 인간승리 노페인 노게인 !!! <img alt="Vue logo" class="logo" src="./assets/img/nopain.png" width="128" height="118" /></p>
-    <p>인소라 짱먹으로 왔다능!!!!!</p>
+    <p>인소라 짱먹으로 왔다능!!!!! <br> <img src="./assets/img/umhahah.png" alt=""> </p>
   </div>
 </template>
 
@@ -25,3 +25,7 @@ header {
 }
 
 </style>
+
+<script>
+
+</script>
